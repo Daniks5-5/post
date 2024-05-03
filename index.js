e110c0fb2604b6995e13eef85e44d2a168f80573
Danil <you@gmail.com>
2024-05-03 21:12:30 +0300

    beginning

---
+++ index.js
@@ -0,0 +1,11 @@
+let postTitle = '';
+const titleInputNode = document.getElementById('base-input__js');//ввод текста в заголовок
+const buttonPublic = document.getElementById('base-btn__js');
+
+buttonPublic.addEventListener('click', function(){
+    postTitle = titleInputNode.value; //postTitle равен значению titleInputNode
+    
+    
+    
+
+});
