578ef9caa6289a99482975e4b29f2fcd768fd71a
Danil <you@gmail.com>
2024-05-03 21:24:01 +0300

    add function text

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let postTitle = '';
 const titleInputNode = document.getElementById('base-input__js');//ввод текста в заголовок
 const buttonPublic = document.getElementById('base-btn__js');
+const postNew = document.getElementById('post-js');//заголовок
 
 buttonPublic.addEventListener('click', function(){
     postTitle = titleInputNode.value; //postTitle равен значению titleInputNode
-    
+    postNew.innerText = postTitle;
     
     
 
