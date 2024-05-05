37697d3104e1130f3c243c99c440284d2cc949bb
Danil <you@gmail.com>
2024-05-05 19:38:58 +0300

    add title and text

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
-let post = '';
-const titleInputNode = document.getElementById('base-input__js');//ввод текста в заголовок
+let post = {
+    title:'',
+    text:'',
+};
+const postTitleInputNode = document.getElementById('base-input__js');//ввод текста в заголовок
+const postTextInputNode = document.getElementById('base-input-main__js');//ввод основного текста 
 const buttonPublic = document.getElementById('base-btn__js');
 const postNew = document.getElementById('post-js');//заголовок
 
 buttonPublic.addEventListener('click', function(){
      //получить данные из поля ввода
-     const postFromUser = getPostFronUser();
+     const postFromUser = getPostFromUser();
 
     setPost(postFromUser); //сохранить пост
 
     renderPost(); //отобразить пост
 
 
-    
-    postTitle = titleInputNode.value; //postTitle равен значению titleInputNode
-    postNew.innerText = postTitle;
+   
     
     
 
 });
  //получить данные из поля ввода
-function getPostFronUser(){
-    const post = titleInputNode.value; //создал отдельный post для отображения данных
-    return post;
+function getPostFromUser(){
+    const title = postTitleInputNode.value; //создал отдельный post для отображения данных
+    const text = postTextInputNode.value;
+    return {
+        title: title,
+        text: text
+    };
 }
  //сохранить пост
 function setPost(newPost){
  post = newPost;
 }
+function getPost(){
+    return post;
+}
  //отобразить пост
  function renderPost(){
-    postNew.innerText = post;
+   const post = getPost();
+
+    const postHTML = `
+    <div class = 'post'>
+    <p class = "post-title"> ${post.title} </p>
+    <p class = "post-text"> ${post.text} </p>
+    </div>
+    `;
+    postNew.innerHTML = postHTML;
  }
