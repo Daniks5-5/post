d4650b94007d326a0e29243d792a29d8af129ab6
Danil <you@gmail.com>
2024-05-06 20:42:28 +0300

    add time

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const postTitleInputNode = document.getElementById('base-input__js');//ввод 
 const postTextInputNode = document.getElementById('base-input-main__js');//ввод основного текста 
 const buttonPublic = document.getElementById('base-btn__js');
 const postNew = document.getElementById('post-js');//заголовок
+const postData = document.getElementById('base-data-js'); //дата заголовка
 
 buttonPublic.addEventListener('click', function(){
      //получить данные из поля ввода
@@ -11,6 +12,7 @@ buttonPublic.addEventListener('click', function(){
     addPost(postFromUser); //сохранить пост
 
     renderPosts(); //отобразить пост 
+   
     
 
 });
@@ -18,30 +20,38 @@ buttonPublic.addEventListener('click', function(){
 function getPostFromUser(){
     const title = postTitleInputNode.value; //создал отдельный post для отображения данных
     const text = postTextInputNode.value;
+    const data = new Date();
     return {
         title: title,
-        text: text
+        text: text,
+        data: data,
+     
     };
 }
  //сохранить пост
-function addPost({title, text}){ //добавление в массив "posts" посты
+function addPost({title, text, data}){ //добавление в массив "posts" посты
+
  posts.push({
     title,
     text,
+    data,
  });
 }
-function getPosts(){
+function getPosts(){ //получение постов и их использование
     return posts;
 }
  //отобразить пост
  function renderPosts(){
    const posts = getPosts();
 
+  
+
    let postsHTML = '';
-   posts.forEach(post => { //+=прибавление
+   posts.forEach(post => { //+=прибавление поста(цикл для добавления постов)
     postsHTML+=  ` <div class = 'post'>  
     <p class = "post-title"> ${post.title} </p>
     <p class = "post-text"> ${post.text} </p>
+    <p class = "post-data"> ${post.data} </p>
     </div>
     `;
    });
@@ -49,3 +59,4 @@ function getPosts(){
   
     postNew.innerHTML = postsHTML;
  }
+ 
